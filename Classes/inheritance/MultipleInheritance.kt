d69cd2b74b1e89e29cdf51885ce2b8d447f35142
thisisxitij <xitij10@gmail.com>
2020-07-25 17:26:21 +0530

    Multiple Inheritance by Interface Delegation

--- Classes/inheritance/MultipleInheritance.kt
+++ Classes/inheritance/MultipleInheritance.kt
@@ -5,46 +5,133 @@ package inheritance
 
 // Multiple Inheritance.
 
+
+// creating interfaces
+
+// common human actions
+interface HumanActions{
+    fun eat()
+    fun talk()
+    fun walk()
+    fun run()
+    fun sleep()
+}
+
+
+// some robot actions
+interface RobotActions {
+    fun talk()
+    fun recordVision()
+    fun solveProblems()
+    fun scan()
+
+
+}
+
+interface HumanInfo {
+    val name : String
+    val age : Int
+}
+
+interface RobotInfo {
+    val robotId: Int
+    val robotCpuFreq: Double
+    val robotMemory: Int
+}
+
+
 // creating a class HumanBeing
-open class HumanBeing(val name: String, val age: Int){
+// initialized the properties of HumanInfo interface in primary constructor
+class HumanBeing( override val name: String,  override val age: Int): HumanInfo, HumanActions{
 
     // common human behaviors
-    fun eat() { println("Eating......") }
-    fun talk() { println("Talking......") }
-    fun walk() { println("Walking.....") }
-    fun run() { println("Running......") }
-    fun sleep() { println("Good Night... Sleeping...") }
+    override fun eat() { println("Eating......") }
+    override fun talk() { println("Talking......") }
+    override fun walk() { println("Walking.....") }
+    override fun run() { println("Running......") }
+    override fun sleep() { println("Good Night... Sleeping...") }
 
 }
 
 // now lets  create a class Robot
-open class Robot(val id: Int, val cpuFreq: Double, var memory: Int){
+class Robot(id: Int,  cpuFreq: Double,  memory: Int): RobotActions, RobotInfo{
 
+    override val robotId: Int = id
+    override val robotCpuFreq: Double  = cpuFreq
+    override val robotMemory: Int = memory
     // declaring some  member methods(come basic actions done by a robot)
-    fun talk() { println("talking in robotic voice....") }
-    fun recordVision(){ println(" recording what im seeing.....")}
-    fun solveProblem(){ println("solving the problem.....") }
-    fun scan(){ println("Scanning  environment around me ...!!!") }
+    override fun talk() { println("talking in robotic voice....") }
+    override fun recordVision(){ println(" recording what im seeing.....")}
+    override fun solveProblems(){ println("solving the problem.....") }
+    override fun scan(){ println("Scanning  environment around me ...!!!") }
 
-    override fun toString(): String {
+    fun about(): String{
         return """
-            ID: $id
-            CPU Frequency: $cpuFreq GH
-            Memory: $memory TB
+            ID: $robotId
+            CPU Frequency: $robotCpuFreq GH
+            Memory: $robotMemory TB
         """.trimIndent()
+
     }
+
 }
 
 // Now Im a mad scientist and like before i fused aliens and human and created Aliuman
 // Just like that i want to create the combination of human and robot yesssss... im going to create a cyborg this time
-// but kotlin and java both does't support the multiple inheritance...
-//  so how we can inherit two or more classes
-// for doing so lets leave this code here for some time and i will learn about abstract classes and interfaces first.
-// then i will come back and create our cyborgs
+// but kotlin and java both does't support the multiple inheritance... so how we can get features of both classes
 
+// for doing so lets leave this code here for some time and i will learn about abstract classes and interfaces first.Then i will come back and create our cyborgs
+
+/** soo back here now  after learning interfaces and interface delegation i have some better idea to create our cyborg
+ * A cyborg is which have features of human and a robot
+ * we are going to use interface delegation to add robot and human features to our cyborg class
+ * do so we have to create some interfaces and implement themlets do it
+
+*/
+
+
+class Cyborg(private val human: HumanBeing, private val robot: Robot):
+        HumanActions by human,
+        RobotActions by robot,
+        HumanInfo by human,
+        RobotInfo by robot{
+
+
+
+    // now we have both HumanBeing and Robot features in our Cyborg class
+    // in short we have created cyborgs
+
+    fun about(): String {
+       return """
+           Name:$name-$robotId
+           Cpu: $robotCpuFreq
+           memory: $robotMemory
+           Age: $age
+       """.trimIndent()
+    }
+
+    // must override the talk() method because HumanBeing and Robot both providing implementation of talk ()
+    override fun talk() {
+        human.talk()
+        robot.talk()
+    }
+
+
+}
 
 
 fun main(){
+    /// huff lets create our cyborg now..........
 
+    // creating a human
+    val alex: HumanBeing  = HumanBeing("alex", 19)
+
+    // creating a robot
+    val robotOne: Robot = Robot(100, 20.5, 1000)
+
+
+    // now time to create a cyborg
+    val firstCyborg: Cyborg = Cyborg(alex, robotOne)
+    println(firstCyborg.about())
 
 }
