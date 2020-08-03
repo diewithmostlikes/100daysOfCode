f85493eb200a8dea1df8c68fbe97c704d3a70009
thisisxitij <xitij10@gmail.com>
2020-08-03 23:33:48 +0530

    corrected some mistakes

--- HigherOrderFunctions/RecreatingWith.kt
+++ HigherOrderFunctions/RecreatingWith.kt
@@ -1,11 +1,15 @@
 // recreating "With" a standard library lambda or higher order function in different ways
 
-// A object must be passed of any type.
+// Every Type of object can be passed.
 fun <T> myWith(obj: T, block: T.() -> Unit){
     // Actual object will be modified
     block(obj) // need to pass the object to block()
 }
 
+// todo Recreating "With" so the actual object should not be modified
+
+
+// Recreating "With" so it can be called directly on objects of every Type
 // No need to pass we can call it on Every Type
 fun <T> T.myWithTwo(block: T.()-> Unit): T { // using generic extension lambda function
     block() // object will be modified
@@ -14,7 +18,8 @@ fun <T> T.myWithTwo(block: T.()-> Unit): T { // using generic extension lambda f
 
 
 fun main() {
-    // object remains unmodified
+
+    // object will be  modified
     println("================ List One ====================")
     val listOne: MutableList<Int> = mutableListOf(10, 20 , 30, 40, 50, 60)
     println("Before: $listOne")
@@ -26,9 +31,9 @@ fun main() {
     println("After: $listOne") // modified list
 
 
-    // modified object will be returned
+    // modified object will be returned and modified
     println("================ List Two =====================")
-    val listTwo: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6, 7)
+    val listTwo: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6)
     println("Before: $listTwo")
 
     listTwo.myWithTwo{
