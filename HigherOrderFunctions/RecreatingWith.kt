87e2e5bc678a03c8906d5bafccc906a3a554d825
thisisxitij <xitij10@gmail.com>
2020-08-04 00:22:29 +0530

    With and without reference in lambda in higher order function

--- HigherOrderFunctions/RecreatingWith.kt
+++ HigherOrderFunctions/RecreatingWith.kt
@@ -1,21 +1,28 @@
 // recreating "With" a standard library lambda or higher order function in different ways
 
 // Every Type of object can be passed.
-fun <T> myWith(obj: T, block: T.() -> Unit){
+fun <T> myWith(obj: T, block: T.() -> Unit){ // higher order function
     // Actual object will be modified
     block(obj) // need to pass the object to block()
 }
 
-// todo Recreating "With" so the actual object should not be modified
-
+// Recreating "With" so block do'nt have reference to "this"
+fun <T> myWithTwo(obj: T, block: (T) -> Unit) { // higher order function
+    // Actual object will be modified
+    block(obj) // block declaration will not have  reference of "this"
+    // so we can't access the properties or method of the instance or object without using "it" argument
+    // or without using custom name given to the lambda argument.
+}
 
 // Recreating "With" so it can be called directly on objects of every Type
 // No need to pass we can call it on Every Type
-fun <T> T.myWithTwo(block: T.()-> Unit): T { // using generic extension lambda function
+fun <T> T.myWithThree(block: T.()-> Unit): T { // higher order function
     block() // object will be modified
     return this // returning the modified/unmodified object
 }
 
+
+// todo Recreate "With" that don't modify the Object
 
 fun main() {
 
@@ -24,24 +31,37 @@ fun main() {
     val listOne: MutableList<Int> = mutableListOf(10, 20 , 30, 40, 50, 60)
     println("Before: $listOne")
 
-    myWith(listOne) {
+    myWith(listOne) {// Has reference "this" so we can use properties and methods without specifying  object or lambda argument name
         repeat(3){removeAt(0)} //modifying list
     }
 
     println("After: $listOne") // modified list
 
 
+    println("======================ListTwo=========================")
+    // object will be modified
+    val listTwo: MutableList<Int> = mutableListOf(100, 200, 300, 400, 500, 600)
+    println("Before $listTwo")
+    myWithTwo(listTwo) {list ->
+        // we can't access the properties or method of the instance or object without using "it" argument  or without using custom name given to the lambda argument.
+        repeat(3) {
+            list.removeAt(0)
+        }
+    }
+    println("After:  $listTwo") // modified list
+
+
     // modified object will be returned and modified
     println("================ List Two =====================")
-    val listTwo: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6)
-    println("Before: $listTwo")
+    val listThree: MutableList<Int> = mutableListOf(1, 2, 3, 4, 5, 6)
+    println("Before: $listThree")
 
-    listTwo.myWithTwo{
+    listThree.myWithThree{// Has reference "this" so we can use properties and methods without specifying  object or lambda argument name
         //Modifying list
         repeat(3) { removeAt(0)}  // Removing first 3 elements of the list (Modifying)
     }
 
-    println("After: $listTwo") // modified list
+    println("After: $listThree") // modified list
 
 
 }
