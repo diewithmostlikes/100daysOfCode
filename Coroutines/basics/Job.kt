1ea146ddd84409f4ebd52895d1d367e48456c1b9
thisisxitij <xitij10@gmail.com>
2020-09-22 13:56:29 +0530

    working on job

--- Coroutines/basics/Job.kt
+++ Coroutines/basics/Job.kt
@@ -1,19 +1,73 @@
 package basics
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.*
 
+
+// note this file can contain wrong explanation or code because im working on it !
 /**
  * so what job ?
  * basically the job is anything that can be canceled ( any cancelable thing ) or its basically a background job
  * and has states like active, canceled, completed  etc and can be started and completed !'
  * */
-fun main() = runBlocking<Unit> { // blocking main thread
 
+val job = Job()
+
+// passing job  to context so we can cancel main coroutine and all its children using that job instance !
+fun main() = runBlocking<Unit>(job) { // blocking main thread // Main Coroutine
+
+    handleCancelException(job) // adding exception handler to job which is thrown when we cancel job
+
+    doSomeThing().join() // to launch coroutines in background thread and .join() to wait for the parent coroutine to  done working
+
+
+}
+
+suspend fun doSomeThing(): Job = GlobalScope.launch{
+    launch { // launched in background (also has the scope of whole application
+        delay(3_000)
+        println("new coroutine  !")
+    }
+
+    launch {
+        delay(7_000)
+        println("Another coroutine  !")
+    }
+
+    launch {
+        // lets say i want to cancel  the main and all its running or launched coroutines if the time exceeds more then 5 sec !
+        delay(5_000)
+        println("Ending the program ! ")
+        endProgram()
+    }
+}
+
+suspend fun endProgram() {
+    //job.cancel() // to cancel all the jobs  or coroutines inside the main coroutine
+    // it will through an JobCancellationException
+    // we can handle that exception by setting up a listener on job
+    // job.invokeOnCompletion { }
+
+    // lets through my CustomException
+    job.cancel(CancellationException("Program is taking longer then 5 sec"))
+
+}
+
+
+fun handleCancelException(job :Job) {
+    job.invokeOnCompletion {
+        if (it?.message == null && it?.message == "" ) { // there a function to this kind of condition i forgot it now will have to look for it !
+            println("Unknown Exception !")
+        }
+        println("Job is canceled due to Reason: ${it?.message} ")
+    }
+
+    // i will practice how to handle exception when cancelling job in handlingException.kt file //
+}
+
+// waiting for job
+suspend fun waitForJob() {
     val job  = GlobalScope.launch {
         println("hey im running please wait for me i will take some time  !")
-        delay(10_000)
+        delay(3_000)
         println("Im done you can move on now")
     }
 
@@ -41,3 +95,4 @@ fun jobCancelExample() {
     println("Canceling execution of coroutine: $resultCancel")
 
 }
+
