19b7e0c277963df1fe3129b047ebb051565d378f
thisisxitij <xitij10@gmail.com>
2020-09-24 23:58:44 +0530

    suspend functions

---
+++ Coroutines/basics/SuspendFunction.kt
@@ -0,0 +1,49 @@
+package basics
+import kotlinx.coroutines.*
+
+/**
+ * suspend function
+ */
+
+
+// so what is suspend function
+// basically the function that is only callable from  inside the coroutines and other suspend function or can call other suspend function inside them.
+
+/** what makes them different then normal function*/
+
+// lets see example to understand them
+fun explainSuspendFun(): Job {
+    // lets create a coroutine and launch it
+   return GlobalScope.launch { // here im  launching a coroutine in GlobalScope (explaining in Scope.kt file)
+      launch {
+          mySuspendFunction()  // adn call our first suspend function.
+          // what it does is... tell the thread im a suspend function
+          // and i can take some time to fully execute or to return something..
+          // but don't waste time waiting on me i release you go and check for my brother coroutines and code lines and execute them
+          // when the result well be ready i will tell you and you can come back to me and resume where you left off and  get the result from me
+          // and complete this coroutine where i have been called from.
+          // that's exactly what a suspend function do
+          // and we call all this process : non-blocking
+          println(" Done !")
+      }
+
+       launch {
+           println("above suspend function has not blocked but suspended for some time which mean this coroutine can be executed ! ")
+       }
+
+       println("Awesome Coroutine. ")
+   }
+
+}
+
+suspend fun mySuspendFunction(): String {
+    delay(3_000L) // can call other suspend function from inside other function.
+    return "Done"
+}
+
+
+fun main() = runBlocking<Unit> {
+    val job  = explainSuspendFun()
+
+    job.join() // to keep the jvm alive Note we are working with GlobalScope which has scope of whole application.
+}
